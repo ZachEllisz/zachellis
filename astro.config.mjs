f2bbb994e2f93562558d889f34cced63e8c441c5
Zach Ellis <zacharyellis81@protonmail.com>
2023-06-24 22:26:22 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ZachEllisz.github.io",
+  site: "https://zachellisz.github.io",
   base: "/zachellis",
 });
